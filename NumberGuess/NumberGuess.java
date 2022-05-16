a71cc778e2c0b02e4448df6fffa77025a792bc2c
umittas2000@gmail.com <umittas2000@gmail.com>
2022-05-16 12:49:43 +0300

    Small adjsutments

--- NumberGuess/NumberGuess.java
+++ NumberGuess/NumberGuess.java
@@ -113,7 +113,7 @@ public class NumberGuess {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("New Push1 : Turkce(1) or English(2) ? :");
+        System.out.print("Turkce(1) or English(2) ? : ");
         int language = input.nextInt();
         printHeader(language);
 
